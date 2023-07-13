294847d9dfacdaaa6866b380d1cedf02e5173087
ShruthikaRajan <113581662+ShruthikaRajan@users.noreply.github.com>
2023-07-13 09:21:01 +0530

    Add files via upload

---
+++ Day3/Report.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import './home.css';
+function Report(){
+    return(
+        <body>
+        <section>
+              <div class="container">
+            <h1>Welcome</h1>
+            </div>
+        </section>
+        </body>
+             
+    )
+}
+export default Report;
+
+
